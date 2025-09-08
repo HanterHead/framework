043b2ba68958030b99d85fc15e81a5979951b7eb
Andew Lynx <3dben@mail.ru>
2025-09-08 15:00:26 +0300

    router update

--- engine/Router/Router.php
+++ engine/Router/Router.php
@@ -2,16 +2,27 @@
 
 namespace Engine\Router;
 
-class Router
+class  Router
 {
     public static $routes = [];
 
     public static $route = [];
 
+    public static function getRoutes(): array
+    {
+        return self::$routes;
+    }
+
+    public static function getRoute(): array
+    {
+        return self::$route;
+    }
+
     public static function add($route,$controller,$method = 'GET')
     {
         self::$routes[$route] = compact('controller','method');
     }
+
 }
 
 
